8611b4bc03ec13e95a4ab4db0396107759e5f7aa
Dhanu <parisamdhanush@gmail.com>
2025-09-25 17:31:49 +0530

    Add initial React app structure with essential files and configurations
    - Added logo512.png for app branding.
    - Created manifest.json for PWA support.
    - Added robots.txt for search engine directives.
    - Implemented App.css for styling the main application.
    - Created App.js as the main component of the app.
    - Added App.test.js for basic testing of the App component.
    - Created index.css for global styles.
    - Implemented index.js as the entry point of the application.
    - Added logo.svg for the app logo.
    - Implemented reportWebVitals.js for performance measurement.
    - Set up testing environment with setupTests.js.
    - Added desktop.ini for folder customization.

---
+++ all-files/my-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+let h1 = React.createElement('h1',{}, "this is h1 tag")
+
+let root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(root);
